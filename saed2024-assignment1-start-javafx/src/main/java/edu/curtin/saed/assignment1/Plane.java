d53d1f1ca26ba919797e563a2ba4b73d439441a5
ameliefisher <amelie.fisher@student.curtin.edu.au>
2024-08-19 14:05:01 +0800

    Created Plane Class

--- saed2024-assignment1-start-javafx/src/main/java/edu/curtin/saed/assignment1/Plane.java
+++ saed2024-assignment1-start-javafx/src/main/java/edu/curtin/saed/assignment1/Plane.java
@@ -1,6 +1,53 @@
 package edu.curtin.saed.assignment1;
 
-public class Plane
+public class Plane implements Runnable
 {
+    private int id;
+    private int x_pos;
+    private int y_pos;
+    private Airport departingAirport;
+    private Airport destinationAirport;
+
+    public Plane(int id, int x_pos, int y_pos, Airport departing)
+    {
+        this.id = id;
+        this.x_pos = x_pos;
+        this.y_pos = y_pos;
+        this.departingAirport = departing;
+    }
+
+    // setters & getters
     
+    // will have to receive from flight request
+    public void setDestinationAirport(Airport destination)
+    {
+        this.destinationAirport = destination;
+    }
+
+    public void setX(int x)
+    {
+        this.x_pos = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y_pos = y;
+    }
+
+    public int getX()
+    {
+        return x_pos;
+    }
+
+    public int getY()
+    {
+        return y_pos;
+    }
+
+    // need to override runnable's run()
+    @Override
+    public void run()
+    {
+        // TO DO: add flight logic here;
+    }
 }
