b1695497d76e597ddf2cafc767b78b50a9291d4e
ameliefisher <amelie.fisher@student.curtin.edu.au>
2024-08-20 12:50:57 +0800

    setup flight request class

--- saed2024-assignment1-start-javafx/src/main/java/edu/curtin/saed/assignment1/FlightRequest.java
+++ saed2024-assignment1-start-javafx/src/main/java/edu/curtin/saed/assignment1/FlightRequest.java
@@ -1,15 +1,46 @@
 package edu.curtin.saed.assignment1;
 import java.util.random.*;
+import java.util.*;
+import java.io.*;
 
 public class FlightRequest implements Runnable
 {
-    private final int airportCount;
-    private final int airportID;
+    private int airportCount; // command input
+    private int originAirportID; // command input
+    private Process proc; // for running FQ
+    private BufferedReader reader; // for reading command
 
-    public FlightRequest(int numberOfAirports, int originAirportID) {
+    // constructor
+    public FlightRequest(int numberOfAirports, int originAirportID)
+    {
         this.airportCount = numberOfAirports;
-        this.airportID = originAirportID;
+        this.originAirportID = originAirportID;
     }
+
     @Override
-    public void run() {}
+    public void run()
+    {
+        try
+        {
+            proc = Runtime.getRuntime().exec(
+                new String[]{"comms/bin/saed_flight_requests",
+                             String.valueOf(airportCount),
+                             String.valueOf(originAirportID)});
+                             
+            reader = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+            String line;
+            
+            while ((line = reader.readLine()) != null)
+            {
+                int destinationAirportID = Integer.parseInt(line);
+                Airport destinationAirport = null; // TO DO: add a way to get destination airports
+
+                // TO DO: add a way to assign FQ to airport's planes
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
